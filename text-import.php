eb0554bfc474b8cf187670024df8ead887c74dbf
mackliu <mackliu@wda.gov.tw>
2023-02-21 16:19:29 +0800

    PHP的文字檔匯入匯出功能

--- text-import.php
+++ text-import.php
@@ -1,4 +1,5 @@
 <?php
+include_once "./api/base.php";
 /****
  * 1.建立資料庫及資料表
  * 2.建立上傳檔案機制
@@ -8,6 +9,24 @@
  * 6.寫入資料庫
  * 7.結束檔案
  */
+
+
+    if(!empty($_FILES['doc']['tmp_name'])){
+        move_uploaded_file($_FILES['doc']['tmp_name'], './upload/'.$_FILES['doc']['name']);
+        $file='./upload/'.$_FILES['doc']['name'];
+    
+        $resource=fopen($file,'r');
+        $line=fgets($resource);
+        while($line=fgets($resource)){
+            $cols=explode(",",$line);
+            $cols[2]=trim(trim($cols[2]),"'");
+            $cols[3]=trim(trim($cols[3]),"'");
+            $cols[4]=trim(trim($cols[4]),"'");
+            insert('students',['name'=>$cols[2],
+                                'no'=>$cols[4],
+                                'birthday'=>$cols[3]]);
+        }
+    }
 
 ?>
 <!DOCTYPE html>
@@ -21,13 +40,47 @@
 </head>
 <body>
 <h1 class="header">文字檔案匯入練習</h1>
-<!---建立檔案上傳機制--->
+<form action="?" method='post' enctype="multipart/form-data">
+    <input type="file" name="doc" id="doc">
+    <input type="submit" value="上傳">
+
+</form>
+
 
 
 
 <!----讀出匯入完成的資料----->
+<?php
+$rows=all('students');
+foreach($rows as $row){
+    echo $row['name']. "-".$row['no']."-".$row['birthday'];
+    echo "<br>";
+}
 
+?>
 
+<!--匯出資料-->
+<?php
+if(file_exists('export.csv')){
+    unlink('export.csv');
+}else{
+    $file=fopen('export.csv','w+');
+    $rows=all('students'," limit 5");
+    fwrite($file,"\xEF\xBB\xBF");
+
+    foreach($rows as $row){
+        $str=join(",",$row);
+        fwrite($file,$str);
+        fwrite($file,"\r\n");
+    }
+
+    fclose($file);
+
+}
+
+?>
+
+<a href='export.csv' download>檔案下載</a>
 
 </body>
 </html>
