1ea244b83b3df86c9e2749be26e13fa4f9cc7164
mackliu <mackliu@wda.gov.tw>
2023-03-01 09:52:16 +0800

    加上亂數間隔距離及不固定的y軸位置

--- cert.php
+++ cert.php
@@ -15,22 +15,23 @@
  * 2.可能有數,大小寫英文
  */
 
-echo cert_to_png(3,5);
+echo cert_to_png(6,9);
 
 function cert_to_png($start,$length){
     $cert=cert_str($start,$length);
 
     $png=imagecreatetruecolor(200,50);
     $white=imagecolorallocate($png,255,255,255);
-    imagestring($png,5,10,10,$cert,$white);
-
-
+    $x_start=10;
+    for($i=0;$i<strlen($cert);$i++){
+        $c=mb_substr($cert,$i,1);
+        imagestring($png,5,$x_start,rand(10,30),$c,$white);
+        $gap=rand(5,15);
+        $x_start=$x_start+10+$gap;
+    }
     imagepng($png,'./cert.png');
-
-
-//    return $cert;
+    return $cert;
 }
-
 
 
 function cert_str($start,$length){
